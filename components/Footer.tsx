bb251ad036731b058cf31abc54c80ef5be0d5b29
Fransi Mengesha <74135451+francyayele@users.noreply.github.com>
2024-09-16 20:55:24 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,13 +9,13 @@ import {
 const Footer = () => {
   return (
     <div className="hidden mdl:inline-flex xl:hidden items-center justify-center w-full py-6 gap-4">
-      <a href="https://github.com/noorjsdivs" target="_blank">
+      <a href="https://github.com/francyayele" target="_blank">
         <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
           <TbBrandGithub />
         </span>
       </a>
       <a
-        href="https://www.youtube.com/channel/UChkOsij0dhgft0GhHRauOAA"
+        href="https://www.youtube.com/channel/UCwYPC9t3mHM6L7FDwVh1Pew"
         target="_blank"
       >
         <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
@@ -23,19 +23,19 @@ const Footer = () => {
         </span>
       </a>
       <a
-        href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+        href="https://www.linkedin.com/in/fransi-mengesha/"
         target="_blank"
       >
         <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
           <SlSocialLinkedin />
         </span>
       </a>
-      <a href="https://www.facebook.com/Noorlalu143/" target="_blank">
+      <a href="https://www.facebook.com/francy.ayele?mibextid=ZbWKwL" target="_blank">
         <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
           <SlSocialFacebook />
         </span>
       </a>
-      <a href="https://www.instagram.com/simplenoor143/" target="_blank">
+      <a href="https://www.instagram.com/francy_ayele/?igsh=ZXFjamQ3ZXJrbmMw" target="_blank">
         <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
           <SlSocialInstagram />
         </span>
