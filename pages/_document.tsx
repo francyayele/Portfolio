eb6aed5bf86a370b58976f36cceef60326840675
Fransi Mengesha <Francyayele@gmail.com>
2025-01-03 22:50:25 +0300

    new

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,10 +11,17 @@ export default function Document() {
           rel="stylesheet"
         />
         <meta name="theme-color" content="#0A192F" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <link rel="manifest" href="/manifest.json" />
         <style>
           {`
             body {
               background-color: #0A192F;
+            }
+            :root {
+              color-scheme: dark;
             }
           `}
         </style>
